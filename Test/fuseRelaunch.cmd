eb0c086e81622ae9e2123bb373df60b151774120
shenmishajing <shenmishajing@Gmail.com>
2019-07-12 04:19:59 +0800

    2019/07/12 04:19

--- Test/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/Verilog/Test/Test_isim_beh.exe" -prj "Z:/Verilog/Test/Test_beh.prj" "work.Test" "work.glbl" 
